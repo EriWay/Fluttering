b946b7ddddd7593462d6c6910c2d38d7a42f6c33
Noe <noe.delvaux@free.fr>
2024-03-28 10:37:29 +0100

    :shovel: Jardinement

--- lib/jardin.dart
+++ lib/jardin.dart
@@ -27,46 +27,79 @@ class Jardin extends StatelessWidget{
           Column(
             children: [
               const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Stack(
                 children: [
-                  Image.asset('Fleur'+1.toString()+".png"),
-                  Image.asset(''),
-                  Image.asset(''),
-                ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0x00000000),
+                          shadowColor: const Color(0x00000000),
+                          surfaceTintColor: const Color(0x00000000),
+                          foregroundColor: const Color(0x00000000),
+                        ),
+                        onPressed: (){}, 
+                        
+                        child: Image.asset('assets/Fleur${0}${0}.png')
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0x00000000),
+                            shadowColor: const Color(0x00000000),
+                            surfaceTintColor: const Color(0x00000000),
+                          ),
+                          onPressed: (){}, 
+                          child: Image.asset('assets/Fleur${0}${0}.png')
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0x00000000),
+                            shadowColor: const Color(0x00000000),
+                            surfaceTintColor: const Color(0x00000000),
+                          ),
+                          onPressed: (){}, 
+                          child: Image.asset('assets/Fleur${0}${0}.png')
+                        ),
+                    ],
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0x00000000),
+                          shadowColor: const Color(0x00000000),
+                          surfaceTintColor: const Color(0x00000000),
+                        ),
+                        onPressed: (){}, 
+                        child: Image.asset('assets/Pot.png')
+                        ),
+                        ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0x00000000),
+                          shadowColor: const Color(0x00000000),
+                          surfaceTintColor: const Color(0x00000000),
+                        ),
+                        onPressed: (){}, 
+                        child: Image.asset('assets/Pot.png')
+                        ),
+                        ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0x00000000),
+                          shadowColor: const Color(0x00000000),
+                          surfaceTintColor: const Color(0x00000000),
+                        ),
+                        onPressed: (){}, 
+                        child: Image.asset('assets/Pot.png')
+                        ),
+                    ],
+                  ),
+                ]
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0x00000000),
-                      shadowColor: const Color(0x00000000),
-                      surfaceTintColor: const Color(0x00000000),
-                    ),
-                    onPressed: (){}, 
-                    child: Image.asset('assets/Pot.png')
-                    ),
-                    ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0x00000000),
-                      shadowColor: const Color(0x00000000),
-                      surfaceTintColor: const Color(0x00000000),
-                    ),
-                    onPressed: (){}, 
-                    child: Image.asset('assets/Pot.png')
-                    ),
-                    ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0x00000000),
-                      shadowColor: const Color(0x00000000),
-                      surfaceTintColor: const Color(0x00000000),
-                    ),
-                    onPressed: (){}, 
-                    child: Image.asset('assets/Pot.png')
-                    ),
-                ],
-              ),
+              
+              
               Image.asset('assets/etagewe.png'),
               const Spacer(),
               Row(
