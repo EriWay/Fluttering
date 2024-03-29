ae0c6139765b93621355a94a1f259e42eb0242b3
PAIN <anthoninpain@hotmail.fr>
2024-03-29 08:48:28 +0100

    ✨clean

--- lib/BDD.dart
+++ lib/BDD.dart
@@ -34,7 +34,7 @@ class BDD {
         String formattedDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
         await database.rawInsert(
             'INSERT OR REPLACE INTO Notes(num_utilisateur, date, humeur, image, vocal, texte) VALUES(?, ?, ?, ?, ?, ?)',
-            [1, formattedDate, '90', 'image.jpg', 'audio.mp3', 'Texte de la note']);
+            [1, formattedDate, '0', 'image.jpg', 'audio.mp3', 'Aujourd hui je suis allé en cours et j ai eu une interro de graphes']);
 
         await database.rawInsert(
             'INSERT INTO User(nom, prenom, Identifiant, mail, num_telephone, mdp, PIN) VALUES(?, ?, ?, ?, ?, ?, ?)',
